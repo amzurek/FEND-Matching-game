b4834b54d793f9c153b42606080e580090884208
Arkadiusz Zurek <arkadiuszmarekzurek@gmail.com>
2018-04-01 02:03:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+#MEMORY GAME
+
+##About
+
+Simple game to test users memory. There are 16 cards and 8 pairs of the same icons. You must find all pairs.
+Try to be the best!
+
+When you find 8 pairs you will see statistic of your game.
+
+##Instructions
+
+1. Cards will mix automatic.
+2. Turn over any two cards.
+3. If the two cards match, they will keep and light on green color.
+4. Remember what was on each card and where it was.
+5. The game is over when all the cards have been matched.
+6. The player with the best time wins.
+
+**CHALLENGE ACCEPTED?**
+
+##License
+
+Copyright 2018 by amzurek
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+**Background was take from [Free Desktop Wallpapers](https://desktopwalls.net/funny/star-wars-darth-vader-orange-juice-minimalistic-desktop-wallpaper)
+
+and (https://www.pinterest.co.uk/pin/554787247829455784/?autologin=true).
+
+### Table of Contents
+
+* [Instructions](#instructions)
+* [Contributing](#contributing)
+
+### Instructions
+
+The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+
+To get started, open `js/app.js` and start building out the app's functionality
+
+For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+
+### Contributing
+
+This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+
+For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
---
+++ js/app.js
@@ -0,0 +1,179 @@
+/*ADD: display the cards on the page, hold list*/
+var card = document.getElementsByClassName('card');
+var cards = [...card];
+var deck = document.getElementById('deckCards');
+var counter = document.querySelector('.moves');
+var moves = 0;
+var matchedCard = document.getElementsByClassName('match');
+var modal = document.getElementById('showSlide');
+var openedCards = [];
+var stars = document.querySelectorAll('.fa-star');
+
+/*ADD: shuffle cards*/
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+
+/*ADD: shuffles cards when page is refresh*/
+document.body.onload = startGame();
+
+/*ADD: start a new game, reset moves, rating, time*/
+function startGame() {
+    cards = shuffle(cards);
+    for ( var i = 0; i < cards.length; i++ ) {
+        deck.innerHTML = '';
+        [].forEach.call(cards, function(item) {
+            deck.appendChild(item);
+        });
+        cards[i].classList.remove('show', 'open', 'match', 'disabled');
+    }
+    moves = 0;
+    counter.innerHTML = moves;
+    for (var i= 0; i < stars.length; i++) {
+        stars[i].style.visibility = 'visible';
+    }
+    second = 0;
+    minute = 0;
+    var timer = document.querySelector('.timer');
+    timer.innerHTML = '0 min 0 sec';
+    clearInterval(interval);
+}
+
+/*ADD: timer*/
+var second = 0, minute = 0;
+var timer = document.querySelector('.timer');
+var interval;
+function startTimer() {
+    interval = setInterval(function() {
+        timer.innerHTML = minute+'min '+second+'sec';
+        second++;
+        if(second === 60) {
+            minute++;
+            second=0;
+        }
+    },1000);
+}
+
+/*ADD: show class to display cards*/
+var displayCard = function() {
+    this.classList.toggle('open');
+    this.classList.toggle('show');
+    this.classList.toggle('disabled');
+};
+
+/*ADD: opened cards list, check cards are match or not*/
+function cardOpen() {
+    openedCards.push(this);
+    var long = openedCards.length;
+    if(long === 2) {
+        moveCounter();
+        if(openedCards[0].type === openedCards[1].type) {
+            matched();
+        } else {
+            unmatched();
+        }
+    }
+}
+
+/*ADD: matched cards*/
+function matched() {
+    openedCards[0].classList.add('match', 'disabled');
+    openedCards[1].classList.add('match', 'disabled');
+    openedCards[0].classList.remove('show', 'open', 'no-event');
+    openedCards[1].classList.remove('show', 'open', 'no-event');
+    openedCards = [];
+}
+
+/*ADD: cards didn't match*/
+function unmatched() {
+    openedCards[0].classList.add('unmatched');
+    openedCards[1].classList.add('unmatched');
+    disable();
+        setTimeout(function() {
+            openedCards[0].classList.remove('show', 'open', 'no-event','unmatched');
+            openedCards[1].classList.remove('show', 'open', 'no-event','unmatched');
+            enable();
+            openedCards = [];
+    },1000);
+}
+
+/*ADD: disable cards*/
+function disable() {
+    Array.prototype.filter.call(cards, function(card) {
+        card.classList.add('disabled');
+    })
+}
+
+/*ADD: enable cards and disable matched cards*/
+function enable() {
+    Array.prototype.filter.call(cards, function(card) {
+        card.classList.remove('disabled');
+        for(var i = 0; i < matchedCard.length; i++) {
+            matchedCard[i].classList.add('disabled');
+        }
+    })
+}
+
+/*ADD: count moves, start timer after first click*/
+function moveCounter() {
+    moves++;
+    counter.innerHTML = moves;
+    if(moves === 1) {
+        second = 0;
+        minute = 0;
+        startTimer();
+    }
+    if (moves > 10 && moves < 14) {
+        for( i= 0; i < 3; i++) {
+            if(i > 1) {
+                stars[i].style.visibility = 'collapse';
+            }
+        }
+    }
+    else if (moves > 15) {
+        for(i= 0; i < 3; i++) {
+            if(i > 0){
+                stars[i].style.visibility = 'collapse';
+            }
+        }
+    }
+}
+
+/*ADD: congratulations popup*/
+function congratulations() {
+    if (matchedCard.length === 16) {
+        clearInterval(interval);
+        finalTime = timer.innerHTML;
+        modal.classList.add('show');
+
+        var starRating = document.querySelector('.stars').innerHTML;
+        document.getElementById('finalMove').innerHTML = moves;
+        document.getElementById('starRating').innerHTML = starRating;
+        document.getElementById('totalTime').innerHTML = finalTime;
+    }
+}
+
+/*ADD: event listeners to cards*/
+for (var i = 0; i < cards.length; i++) {
+    card = cards[i];
+    card.addEventListener("click", displayCard);
+    card.addEventListener("click", cardOpen);
+    card.addEventListener("click", congratulations);
+}
+
+/*ADD: play again function*/
+function playAgain() {
+    modal.classList.remove("show");
+    startGame();
+}
+
+
